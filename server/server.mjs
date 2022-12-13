6f650bf5f192a68fc5752fb16193431e64e3c0cb
phiG1tHub <45313155+phiG1tHub@users.noreply.github.com>
2022-12-13 13:47:54 +0100

    fix static

---
+++ server/server.mjs
@@ -0,0 +1,27 @@
+import express from 'express';
+import path from 'path';
+
+const dirname = path.dirname(process.argv[1])
+const host = 'localhost';
+
+
+const server = express();
+
+server.use(express.static(path.join(dirname ,'../dist/')));
+
+server.get("/", (request, response) => {
+    response.sendFile();
+});
+
+// grabbing the port from the commandline or setting 8080 as default
+
+let port = process.argv.slice(2)[0];
+
+if (port == null) {
+    port = 8080;
+} else {
+    port = parseInt(port);
+}
+
+// listening for requests
+server.listen(port, host, () => { console.log('Server is running on http://' + host + ':' + port); });
