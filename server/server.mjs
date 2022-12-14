f412887bd1ef87e969687a201b7753aeeccef210
phiG1tHub <phi.business@protonmail.com>
2022-12-14 12:51:01 +0100

    Fix lint script

--- server/server.mjs
+++ server/server.mjs
@@ -1,26 +1,25 @@
 import express from 'express';
 import path from 'path';
 
-const dirname = path.dirname(process.argv[1])
+const dirname = path.dirname(process.argv[1]);
 const host = 'localhost';
-
 
 const server = express();
 
-server.use(express.static(path.join(dirname ,'../dist/')));
+server.use(express.static(path.join(dirname, '../dist/')));
 
-server.get("/", (request, response) => {
-    response.sendFile(path.join(dirname,'../dist/index.html'));
+server.get('/', (request, response) => {
+  response.sendFile(path.join(dirname, '../dist/index.html'));
 });
 
 // grabbing the port from the commandline or setting 8080 as default
 
 let port = process.argv.slice(2)[0];
 
-if ( port == null ) {
-    port = 8080;
+if (port == null) {
+  port = 8080;
 } else {
-    port = parseInt( port );
+  port = parseInt(port);
 }
 
 // listening for requests
