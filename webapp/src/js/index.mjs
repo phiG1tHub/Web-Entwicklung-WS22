29e524f48710e51cf4ec8d8a25d85c58ade83635
phiG1tHub <45313155+phiG1tHub@users.noreply.github.com>
2022-12-25 17:36:03 +0100

    Draft, SPA boilerplate

---
+++ webapp/src/js/index.mjs
@@ -0,0 +1,64 @@
+/* eslint-env browser */
+import Dashboard from '../views/Veranstalltungen.mjs';
+import Posts from '../views/Posts.mjs';
+import PostView from '../views/PostView.mjs';
+import Settings from '../views/Settings.mjs';
+
+const pathToRegex = path => new RegExp('^' + path.replace(/\//g, '\\/').replace(/:\w+/g, '(.+)') + '$');
+
+const getParams = match => {
+  const values = match.result.slice(1);
+  const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(result => result[1]);
+
+  return Object.fromEntries(keys.map((key, i) => {
+    return [key, values[i]];
+  }));
+};
+
+const navigateTo = url => {
+  history.pushState(null, null, url);
+  router();
+};
+
+const router = async () => {
+  const routes = [
+    { path: '/', View: Dashboard },
+    { path: '/posts', View: Posts },
+    { path: '/posts/:id', View: PostView },
+    { path: '/settings', View: Settings }
+  ];
+
+  // Test each route for potential match
+  const potentialMatches = routes.map(route => {
+    return {
+      route: route,
+      result: location.pathname.match(pathToRegex(route.path))
+    };
+  });
+
+  let match = potentialMatches.find(potentialMatch => potentialMatch.result !== null);
+
+  if (!match) {
+    match = {
+      route: routes[0],
+      result: [location.pathname]
+    };
+  }
+
+  const view = new match.route.View(getParams(match));
+
+  document.querySelector('#app').innerHTML = await view.getHtml();
+};
+
+window.addEventListener('popstate', router);
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.addEventListener('click', e => {
+    if (e.target.matches('[data-link]')) {
+      e.preventDefault();
+      navigateTo(e.target.href);
+    }
+  });
+
+  router();
+});
---
+++ webapp/src/views/Veranstalltungen.mjs
@@ -0,0 +1,33 @@
+import AbstractView from './AbstractView.mjs';
+// import uikit from 'uikit/src/js/uikit.js';
+
+export default class extends AbstractView {
+  constructor (params) {
+    super(params);
+    this.setTitle('Veranstalltungen');
+  }
+
+  async getHtml () {
+    // uikit.showModal();
+    return `
+<table class="uk-table">
+    <caption>Veranstalltungen</caption>
+    <thead>
+        <tr>
+            <th>Name</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td></td>
+        </tr>
+    </tbody>
+    <tfoot>
+        <tr>
+            <td></td>
+        </tr>
+    </tfoot>
+</table>
+`;
+  }
+}
